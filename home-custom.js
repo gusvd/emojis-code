92ff5228eaf669f424760e6d00463a95a5b288ff
Gus VD <gustavo@crewmates.co>
2022-09-06 17:20:01 +0100

    custom file

---
+++ home-custom.js
@@ -0,0 +1,38 @@
+const searchClient = algoliasearch('OCUTWK8OFD', '721e9f1972bc892174d0794ddc9a306d');
+
+const search = instantsearch({
+  indexName: 'weheartemojis',
+  searchClient,
+  searchParameters: {
+    //query: 'lenovo',
+    filters: 'brand:Samsung OR brand:Apple',
+  },
+});
+
+search.addWidgets([
+ instantsearch.widgets.configure({
+    hitsPerPage: 64
+  }),
+  instantsearch.widgets.searchBox({
+    container: '#input-container',
+    placeholder: 'Search emojis, symbols, occasions, emotions...',
+    autofocus: true,
+    showSubmit: false,
+    cssClasses: {
+    	input: 'search-input',
+    }
+	}),
+	instantsearch.widgets.hits({
+   container: '#results-container',
+   cssClasses: {
+    root: 'results-wrapper',
+    list: 'emoji-wrapper',
+    item: 'emoji',
+   },
+   templates: {
+     item: `{{ symbol }}`
+    }
+  })
+]);
+
+search.start();
