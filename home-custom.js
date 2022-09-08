13edd1e310656b6bed98230f0b4a59fe68fbb0b6
Gus VD <gustavo@crewmates.co>
2022-09-08 15:59:57 +0100

    added link to cpy paste

--- home-custom.js
+++ home-custom.js
@@ -30,7 +30,17 @@ search.addWidgets([
     item: 'emoji',
    },
    templates: {
-     item: `{{ symbol }}`
+     item: `
+     <a
+     href="#"
+     class="emoji-link"
+     style="
+       transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg)
+         rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+       transform-style: preserve-3d;
+     ">
+     {{ symbol }}
+     </a>`
     }
   })
 ]);
