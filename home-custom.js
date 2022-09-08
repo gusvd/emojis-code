38d65ade1a86cba1d9b4e7542b15df21cf250225
Gus VD <gustavo@crewmates.co>
2022-09-08 16:02:51 +0100

    clipboard.js

--- home-custom.js
+++ home-custom.js
@@ -1,3 +1,6 @@
+////////////////////////////////////////////////
+// Algolia
+////////////////////////////////////////////////
 const searchClient = algoliasearch('OCUTWK8OFD', '721e9f1972bc892174d0794ddc9a306d');
 
 const search = instantsearch({
@@ -46,3 +49,27 @@ search.addWidgets([
 ]);
 
 search.start();
+
+
+////////////////////////////////////////////////
+// clipboard.js
+////////////////////////////////////////////////
+
+var clipboard = new ClipboardJS('.emoji-link', {
+    target: function(trigger) {
+        return trigger;
+    }
+});
+  
+  clipboard.on('success', function(e) {
+    console.info('Action:', e.action);
+    console.info('Text:', e.text);
+    console.info('Trigger:', e.trigger);
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function(e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
