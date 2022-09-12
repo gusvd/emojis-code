2042ef60838eaf91fa934ee35fc047e786212bf6
Gus VD <gustavo@crewmates.co>
2022-09-12 13:22:16 +0100

    s

--- home-custom.js
+++ home-custom.js
@@ -2,47 +2,51 @@
 // Algolia
 ////////////////////////////////////////////////
 const searchClient = algoliasearch(
-  "OCUTWK8OFD",
-  "721e9f1972bc892174d0794ddc9a306d"
+  'OCUTWK8OFD',
+  '721e9f1972bc892174d0794ddc9a306d'
 );
+
 const search = instantsearch({
-  indexName: "emojis",
+  indexName: 'emojis',
   searchClient,
   searchParameters: {
-    query: "hearts",
-    //filters: 'brand:Samsung OR brand:Apple',
+    Query: 'hearts',
   },
 });
-search.addWidget([
+
+search.addWidgets([
+ instantsearch.widgets.configure({
+    hitsPerPage: 96
+  }),
   instantsearch.widgets.searchBox({
-    container: "#input-container",
-    placeholder: "Search emojis, symbols, occasions, emotions...",
+    container: '#input-container',
+    placeholder: 'Search emojis, symbols, occasions, emotions...',
     autofocus: true,
     showSubmit: false,
     showReset: false,
     cssClasses: {
-      input: "search-input",
-    },
-  }),
-  instantsearch.widgets.hits({
-    container: "#search-results",
-    cssClasses: {
-      root: "results-wrapper",
-      list: "emoji-wrapper",
-      item: "emoji",
-    },
-    templates: {
-      item: `
-      <a
-      href="#"
-      class="emoji-link"
-      style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg)
-          rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
-        transform-style: preserve-3d;
-      ">
-      {{ symbol }}
-      </a>`,
-    },
+    	input: 'search-input',
+    }
+	}),
+    instantsearch.widgets.hits({
+   container: '#search-results',
+   cssClasses: {
+    root: 'results-wrapper',
+    list: 'emoji-wrapper',
+    item: 'emoji',
+   },
+   templates: {
+     item: `
+     <a
+     href="#"
+     class="emoji-link"
+     style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg)
+         rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
+       transform-style: preserve-3d;
+     ">
+     {{ symbol }}
+     </a>`
+    }
   })
 ]);
 
