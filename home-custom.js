f05df0f8d88afb14e6d233c401494dea094e52aa
Gus VD <gustavo@crewmates.co>
2022-09-12 13:12:47 +0100

    test

--- home-custom.js
+++ home-custom.js
@@ -5,91 +5,26 @@ const searchClient = algoliasearch(
   'OCUTWK8OFD',
   '721e9f1972bc892174d0794ddc9a306d'
 );
-
 const search = instantsearch({
   indexName: 'emojis',
   searchClient,
   searchParameters: {
-    Query: 'hearts',
+    query: 'hearts',
+    //filters: 'brand:Samsung OR brand:Apple',
   },
 });
-
-search.addWidgets([
- instantsearch.widgets.configure({
-    hitsPerPage: 96
-  }),
-  instantsearch.widgets.searchBox({
-    container: '#input-container',
-    placeholder: 'Search emojis, symbols, occasions, emotions...',
-    autofocus: true,
-    showSubmit: false,
-    showReset: false,
-    cssClasses: {
-    	input: 'search-input',
-    }
-	}),
-	instantsearch.widgets.hits({
-   container: '#search-results',
-   cssClasses: {
-    root: 'results-wrapper',
-    list: 'emoji-wrapper',
-    item: 'emoji',
-   },
-   templates: {
-     item: `
-     <a
-     href="#"
-     class="emoji-link"
-     style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg)
-         rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
-       transform-style: preserve-3d;
-     ">
-     {{ symbol }}
-     </a>`
-    }
+search.addWidget(
+  instantsearch.widgets.hits({
+    container: '#search-results',
+    templates: {
+      item: `
+{{ symbol }}
+`,
+    },
   })
-]);
+);
 
 search.start();
-
-////////////////////////////////////////////////
-// Algolia categories query
-////////////////////////////////////////////////
-
-// const searchAnimals = instantsearch({
-//   indexName: 'emojisandsymbols',
-//   searchClient,
-//   searchParameters: {
-//     Query: 'animals',
-//   },
-// });
-// searchAnimals.addWidgets([
-//   // instantsearch.widgets.configure({
-//   //   hitsPerPage: 48
-//   // }),
-// 	instantsearch.widgets.hits({
-//    container: '#animals',
-//    cssClasses: {
-//     root: 'results-wrapper',
-//     list: 'emoji-wrapper',
-//     item: 'emoji',
-//    },
-//    templates: {
-//      item: `
-//      <a
-//      href="#"
-//      class="emoji-link"
-//      style="transform: translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg)
-//          rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg);
-//        transform-style: preserve-3d;
-//      ">
-//      {{ symbol }}
-//      </a>`
-//     }
-//   })
-// ]);
-
-// searchAnimals.start();
 
 
 ////////////////////////////////////////////////
